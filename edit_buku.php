f630dbd3fd5e4200c0c5554c39adbdfbcb74bb3b
Yusuf <yusufjana18@gmail.com>
2023-09-22 02:02:08 +0700

    feature edit buku

---
+++ edit_buku.php
@@ -0,0 +1,26 @@
+<?php 
+    require_once "koneksi.php";
+    $id = $_GET['id'];
+    $query = mysqli_query($koneksi, "SELECT * FROM buku WHERE id='$id'");
+    $data = mysqli_fetch_array($query);
+    $id = $data['id'];
+    $judul = $data['judul'];
+    $pengarang = $data['pengarang'];
+    $penerbit = $data['penerbit'];
+    $tahun = $data['tahun'];
+    $harga = $data['harga'];
+    $stok = $data['stok'];
+    $gambar = $data['gambar'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Buku</title>
+</head>
+<body>
+    
+</body>
+</html>
